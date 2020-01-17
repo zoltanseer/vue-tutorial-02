fd5e59426e0a63719145125f4359682c52c31987
zoltanseer <zoltan.seer@gmail.com>
2020-01-17 23:36:12 +0200

    finished project

---
+++ src/components/Chat.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="chat container">
+    <h2 class="center teal-text">Chat</h2>
+    <div class="card">
+      <div class="card-content">
+        <ul class="messages" v-chat-scroll>
+          <li v-for="msg in messages" :key="msg.id">
+            <span class="teal-text">{{ msg.name }}:</span>
+            <span class="grey-text text-darken-3">{{ msg.content }}</span>
+            <span class="grey-text time">{{ msg.timestamp }}</span>
+          </li>
+        </ul>
+      </div>
+      <div class="card-action">
+        <NewMessage :name="name" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import NewMessage from "@/components/NewMessage";
+import db from "@/firebase/init";
+import moment from "moment";
+export default {
+  name: "Chat",
+  props: ["name"],
+  components: {
+    NewMessage
+  },
+  data() {
+    return {
+      messages: []
+    };
+  },
+  created() {
+    let ref = db.collection("messages").orderBy("timestamp");
+    ref.onSnapshot(snapshot => {
+      snapshot.docChanges().forEach(change => {
+        if (change.type === "added") {
+          let doc = change.doc;
+          this.messages.push({
+            id: doc.id,
+            name: doc.data().name,
+            content: doc.data().content,
+            timestamp: moment(doc.data().timestamp).format("lll")
+          });
+        }
+      });
+    });
+  }
+};
+</script>
+
+<style>
+.chat h2 {
+  font-size: 2.6em;
+  margin-bottom: 40px;
+}
+.messages {
+  max-height: 400px;
+  overflow: auto;
+}
+.messages span {
+  font-size: 1.4em;
+}
+.messages .time {
+  display: block;
+  font-size: 0.8em;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,35 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Chat from "@/components/Chat";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
+    path: "/",
+    name: "home",
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: "/chat",
+    name: "Chat",
+    component: Chat,
+    props: true,
+    beforeEnter: (to, from, next) => {
+      if (to.params.name) {
+        next();
+      } else {
+        next({ name: "home" });
+      }
+    }
   }
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
